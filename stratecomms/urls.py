367a25cfe280d3ec9dcc29b99674628ff76bcdee
bertrandoey <bertrandoey05@gmail.com>
2022-12-08 12:05:31 +0700

    pertama

--- stratecomms/urls.py
+++ stratecomms/urls.py
@@ -19,5 +19,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('jadwalSertifikasi/', views.jadwalSertifikasi, name='jadwalSertifikasi'),
+    path('artikel/', views.artikel, name='artikel'),
+    path('isiArtikel/', views.isiArtikel, name='isiArtikel')
 ]
--- stratecomms/views.py
+++ stratecomms/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'base.html')
+
+def jadwalSertifikasi(request):
+  return render(request,'jadwalSertifikasi.html')
+
+def artikel(request):
+  return render(request, 'artikel.html')
+
+def isiArtikel(request):
+  return render(request, 'isiArtikel.html')
